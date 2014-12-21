dbc916769d1e454a4210512f61bf6ec701e5de23
George <yugeorge@uw.edu>
2014-12-21 05:18:43 -0800

    scrollable added

---
+++ js/scrollable.js
@@ -0,0 +1,10 @@
+/**
+ * Created by George on 12/21/2014.
+ */
+
+
+    // initialize tools.scrollable with configuration variables
+$("#about").scrollable({
+    keyboard: false,
+    circular: true
+});
